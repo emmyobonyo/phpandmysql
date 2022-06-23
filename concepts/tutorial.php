9f47a1ab801445d2f0a1a76fad974221dca6fa1a
unknown <emmyobonyo@gmail.com>
2022-06-23 19:47:46 +0300

    First commit

---
+++ concepts/tutorial.php
@@ -0,0 +1,17 @@
+<?php
+
+    // $str = "Hello and welcome to simply learn"; // declare a variable
+    // $num = 5;
+    // $float = 7.6;
+
+    // echo "<h1>" . $str . "<h1>"; // print a variable
+    // echo "<h2>" . $num . "<h2>";
+    // echo "<h3>" . $float . "<h3>";
+
+    // $laptop_brand = array("HP", "Lenovo", "Mac", "Dell");
+    // echo var_dump($laptop_brand[1]);
+
+    define("laptops", ["HP", "Mac", "Lenovo", "Dell"]);
+    echo laptops[2] 
+
+?>
